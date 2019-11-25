9610ebe847e5113230f68cd27d4f7d537a86b709
Emre <6238977+EmreJA@users.noreply.github.com>
2019-11-25 14:24:57 -0500

    added cleave js and accounting js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-document.getElementById("calcy").addEventListener("click", function lottery() {
-    annual = document.getElementById("lotteryAmount").value * 0.04 ;
-    monthly = annual / 12 ;
-    document.getElementById("finalText").innerHTML = 
-    `<p>If you were to win a <b> $${document.getElementById("lotteryAmount").value} jackpot</b>, you would have:</p>
-    <p><b>An annual income of</b> $${annual}</p>
-    <p><b>A monthly income of</b> $${monthly}</p>
-    <hr>
-    <p><em>This calculation was made by taking the "4% rule of investment" into consideration, which means you will never run out of money if you were to spend the amounts above while keeping the rest of the jackpot in a S&P 500 index fund.</em></p>
+var cleave = new Cleave('.lottery-A', {
+  numeral: true,
+  numeralThousandsGroupStyle: 'thousand'
+});
 
-    <p><em>This calculation only makes sense in the US & Canada since the inflation numbers play a big role in the final results. Your mileage may vary if you live in another country.</em></p>`;
+document.getElementById("calcy").addEventListener("click", function lottery() {
+    lotteryValue = accounting.formatMoney(document.getElementById("lotteryAmount").value);
+    annual = document.getElementById("lotteryAmount").value * 0.04 ;
+    annualResult = accounting.formatMoney(annual);
+    monthly = annual / 12 ;
+    monthlyResult = accounting.formatMoney(monthly);
+
+    document.getElementById("finalText").innerHTML = 
+      `
+      <hr>
+      <p>If you were to win a <b> ${lotteryValue} jackpot</b>, you would have:</p>
+      <p><b>An annual income of</b> ${annualResult}</p>
+      <p><b>A monthly income of</b> ${monthlyResult}</p>
+      <hr>
+      <p><em>This calculation was made by taking the <a href="https://www.investopedia.com/terms/f/four-percent-rule.asp" target="_blank">4% rule of investment</a> into consideration, which means you will never run out of money if you were to spend the amounts above while keeping the rest of the jackpot in a S&P 500 index fund.</em></p>
+
+      <p><em>This calculation only makes sense in the US & Canada since the inflation numbers play a big role in the final results. Your mileage may vary if you live in another country.</em></p>`;
   });
   
